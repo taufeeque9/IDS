ae9a71d5689d5a1f10c14b4877dc80c92b2b9ca9
taufeeque9 <9taufeeque9@gmail.com>
2020-05-24 18:07:53 +0530

    added split_dataset

--- featureSelection.py
+++ featureSelection.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import LabelEncoder
 dataset = pd.read_csv("MachineLearningCVE/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv")
 
 # print(dataset.isnull().sum())
-new_data = dataset.dropna(axis = 0, how ='any', inplace=False)
+new_data = dataset.dropna(axis=0, how='any', inplace=False)
 
 y = new_data[[' Label']]
 X = new_data.drop([' Label'], axis=1, inplace=False)
@@ -28,7 +28,8 @@ features_needed = 15
 
 print("Selection based on Pearson Correlation")
 
-def cor_selector(X, y,num_feats):
+
+def cor_selector(X, y, num_feats):
     cor_list = []
     feature_name = X.columns.tolist()
     # calculate the correlation with y for each feature
@@ -38,10 +39,11 @@ def cor_selector(X, y,num_feats):
     # replace NaN with 0
     cor_list = [0 if np.isnan(i) else i for i in cor_list]
     # feature name
-    cor_feature = X.iloc[:,np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
+    cor_feature = X.iloc[:, np.argsort(np.abs(cor_list))[-num_feats:]].columns.tolist()
     # feature selection? 0 for not select, 1 for select
     cor_support = [True if i in cor_feature else False for i in feature_name]
     return cor_support, cor_feature
+
 
 cor_support, cor_feature = cor_selector(X_new, y, features_needed)
 print(str(len(cor_feature)), 'selected features')
@@ -51,15 +53,15 @@ print(cor_feature)
 
 print("Selection based on chi square distibution")
 
-chi_selector = SelectKBest(chi2, k = features_needed) 
+chi_selector = SelectKBest(chi2, k=features_needed)
 X_kbest_features = chi_selector.fit_transform(X_new, y)
 chi_support = chi_selector.get_support()
-chi_feature = X_new.loc[:,chi_support].columns.tolist()
+chi_feature = X_new.loc[:, chi_support].columns.tolist()
 print(str(len(chi_feature)), 'selected features')
 print(chi_feature)
 
 
-# # More Raw Approach - Leaving the first 2 valued and 
+# # More Raw Approach - Leaving the first 2 valued and
 # # last NaN valued features, all other features are
 # # dependent and can be considered for training
 
